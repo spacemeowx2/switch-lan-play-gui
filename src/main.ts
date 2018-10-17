14572c1c29c6736e8658e5d6bb11b4f9f1dd6529
spacemeowx2 <spacemeowx2@gmail.com>
2018-10-17 18:00:28 +0800

    add vuetify and tslint set to standard extend

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App)
+}).$mount('#app')
